acf7f62ee8b1857a01e63b415519e71f98b2182f
MarkAOF <marco.aof6@gmail.com>
2025-05-28 18:57:58 -0300

    fix: o codico js da pagina agora está funcionando.

--- src/js/combustivel.js
+++ src/js/combustivel.js
@@ -3,11 +3,20 @@ document.addEventListener('DOMContentLoaded', () => {
     if (!form) return;
 
     form.addEventListener('submit', calculateGas);
-})
+});
 
 function calculateGas(e) {
-    const distanc = parseFloat(document.getElementById('distancia').value);
-    const consumo = parseFloat(document.getElementById('consumo-veiculo').value);
-    const preco = parseFloat(document.getElementById('preco-combustivel').value);
+    e.preventDefault();
+    const dist = parseFloat(document.getElementById('distancia').value) || 0;
+    const cons = parseInt(document.getElementById('consumo-vaiculo').value) || 0;
+    const preGas = parseFloat(document.getElementById('preco-combustivel').value) || 0;
 
+    const total = dist * cons * preGas;
+    const formatted = total.toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    });
+
+    document.getElementById('result-combustivel')
+        .innerHTML = `<span>O valor do combustível é: ${formatted}</span>`;
 }
